576a3d864db375717879050a914c98fcd471eba2
AMOHAMMEDIMRAN <amohammedimran.11@gmail.com>
2025-01-03 22:38:57 +0530

    remove

--- backend/routes/petRoute.js
+++ backend/routes/petRoute.js
@@ -16,6 +16,6 @@ const router = express.Router();
 router.route("/").get(getPets);
 router.route("/:id").get(getPetById);
 router.route("/:id").delete(authShelterMiddleware, authShelter, deletePet);
-router.route("/addpet").post( authShelter, addPet);
+router.route("/addpet").post(addPet);
 
 export default router;
